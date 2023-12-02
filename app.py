d8c76a47be69b063a77a67d506ea8eb32bd97931
Ivan Chernenko <64325866+ivchern@users.noreply.github.com>
2023-12-02 16:15:16 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,173 @@
+from flask import Flask, render_template, request, jsonify
+import sqlite3
+import os
+from werkzeug.utils import secure_filename
+from parser_service import MessageFileProcessor
+import re
+import json
+
+app = Flask(__name__)
+DATABASE = 'messages.db'
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'rar'}
+
+
+class MessageDatabase:
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def create_table(self, table_name):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f'''
+                CREATE TABLE IF NOT EXISTS {table_name} (
+                    id INTEGER NOT NULL,  
+                    chat_id TEXT,
+                    file_chat TEXT,
+                    author_name TEXT,
+                    author_link TEXT,
+                    message_text TEXT,
+                    message_date TEXT,
+                    attachment_links TEXT
+                )
+            ''')
+
+    def insert_data(self, json_data, table_name):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            for item in json_data:
+                cursor.execute(f'''
+                    INSERT INTO {table_name} (
+                        id, chat_id, file_chat, author_name, author_link, message_text, message_date, attachment_links
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                ''', (
+                    item['id'],
+                    item['chat_id'],
+                    item['file_chat'],
+                    item['author_name'],
+                    item['author_link'],
+                    item['message_text'],
+                    item['message_date'],
+                    json.dumps(item.get('attachment_links', []))
+                ))
+
+    def commit_and_close(self):
+        pass  # You can keep this method empty since closing the connection after each operation
+
+    def execute_query(self, query):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            results = cursor.fetchall()
+        return results
+
+db = MessageDatabase(DATABASE)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def is_valid_database_name(name):
+    pattern = re.compile('^[0-9a-zA-Z$_]+$')
+    return bool(pattern.match(name))
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+    name_table = str(file.filename).replace('.rar', '').lower()
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if not is_valid_database_name(name_table):
+        return jsonify({'error': 'Rename to database format'})
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        processor = MessageFileProcessor()
+        json_data = processor.process_all_html_files(file_path, app.config['UPLOAD_FOLDER'])
+
+        db.create_table(name_table)
+        db.insert_data(json_data, name_table)
+
+        return jsonify({'success': 'File uploaded and processed successfully'})
+
+    return jsonify({'error': 'Invalid file extension'})
+
+
+@app.route('/')
+def index():
+    table_list = get_table_list()
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return render_template('index.html', table_list=table_list, upload_status='No file part')
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return render_template('index.html', table_list=table_list, upload_status='No selected file')
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+
+            processor = MessageFileProcessor()
+            json_data = processor.process_all_html_files(file_path, 'uploads')
+
+            db.create_table(file.filename.replace('rar', '').lower())
+            db.insert_data(json_data, file.filename.replace('rar', '').lower())
+            db.commit_and_close()
+
+            upload_status = 'File uploaded and processed successfully'
+        else:
+            upload_status = 'Invalid file extension'
+
+        return render_template('index.html', table_list=table_list, upload_status=upload_status)
+
+    return render_template('index.html', table_list=table_list, upload_status=None)
+
+
+@app.route('/query', methods=['POST'])
+def query():
+    user_query = request.form['query']
+    results = db.execute_query(user_query)
+    table_name = extract_table_name(user_query)
+    return render_template('result.html', results=results, my_table=table_name)
+
+def extract_table_name(sql_query):
+    match = re.search(r'FROM\s+(\w+)', sql_query, re.IGNORECASE)
+    return match.group(1) if match else 'default_table'
+
+@app.route('/details', methods=['GET'])
+def details():
+    id = request.args.get('id')
+    table_name = request.args.get('my_table', 'default_table')  # Provide a default table name if not specified
+    if id is not None:
+        query = f"SELECT * FROM {table_name} WHERE ID > {int(id) - 200} AND ID <= {int(id) + 200}"
+        result_data = db.execute_query(query)
+        return render_template('messages.html', results=result_data, selected_id=id)
+
+    return render_template('messages.html', results=[])
+
+
+def get_table_list():
+    with sqlite3.connect(DATABASE) as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+    return [table[0] for table in tables]
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ parser_service.py
@@ -0,0 +1,233 @@
+import tempfile
+from bs4 import BeautifulSoup
+import os
+import glob
+import sqlite3
+import json
+from tqdm import tqdm
+import rarfile
+import shutil
+import re
+
+class MessageParser:
+    def __init__(self):
+        self.date_format = "%d.%m.%Y %H:%M"
+
+    def parse_date_time(self, date_time_str):
+        try:
+            date_time_obj = datetime.strptime(date_time_str, self.date_format)
+            date_only = date_time_obj.date()
+            time_only = date_time_obj.time()
+            return {"date": date_only, "time": time_only}
+        except ValueError as e:
+            return {"error": str(e)}
+        
+    def extract_number(self,file_path):
+        try:
+            return int(''.join(filter(str.isdigit, os.path.basename(file_path))))
+        except ValueError:
+            return float('inf')
+
+class MessageExtractor(MessageParser):
+    def __init__(self):
+        super().__init__()
+
+    def get_author_blocks(self, soup):
+        try:
+            return soup.find('div', class_='wrapped')
+        except Exception as e:
+            print(f'Ошибка при получении блоков: {e}')
+            return None
+
+    def get_im_in_blocks(self, soup):
+        try:
+            return soup.find_all('div', class_='im_in')
+        except Exception as e:
+            print(f'Ошибка при получении блоков: {e}')
+            return []
+
+    def get_name(self, block):
+        try:
+            return block.find('a', class_='mem_link').text.strip()
+        except Exception as e:
+            print(f'Ошибка при получении имени: {e}')
+            return None
+
+    def get_user_id(self, href_value):
+        try:
+            return href_value.replace('https://vk.com/id', '')
+        except Exception as e:
+            print(f'Ошибка при извлечении user_id: {e}')
+            return None
+
+    def get_message_date(self, block):
+        try:
+            date_element = block.find('div', class_='im_log_date').find('a', class_='im_date_link')
+            message_date = date_element.text.strip() if date_element else None
+            return message_date
+        except Exception as e:
+            print(f'Ошибка при извлечении message_date: {e}')
+            return None
+
+    def get_message(self, block):
+        try:
+            message_text = ''.join([str(item) for item in block.contents if isinstance(item, str)])
+            message_text = message_text.strip()
+            return message_text
+        except Exception as e:
+            print(f'Ошибка при получении сообщения: {e}')
+            return None
+
+    def get_attachment_links(self, block): 
+        try: 
+            gallery_attachments = block.find_all('div', class_='gallery attachment')
+            attachment_links = [a['href'] for gallery_attachment in gallery_attachments for a in gallery_attachment.find_all('a', class_='download_photo_type')]
+            return attachment_links
+        except Exception as e:
+            print(f'Ошибка при получении сообщения: {e}')
+            return []
+
+class DetailsChat(): 
+    def __init__(self):
+        super().__init__()
+
+    def details(self, file_path):
+        return {
+            "chat_id": self._get_last_folder(file_path),  
+            "file_chat": str(file_path)
+        }
+    
+    @staticmethod
+    def _get_last_folder(path):
+        folder_name = os.path.basename(os.path.dirname(path))
+        match = re.search(r'\(id(\d+)\)', folder_name)
+        if match:
+            return match.group(1)
+        else:
+            return None
+
+
+class MessageProcessor(MessageExtractor):
+    def __init__(self):
+        super().__init__()
+
+    def process_block(self, block):
+        try:
+            author_block = self.get_author_blocks(block)
+            author_name = self.get_name(author_block)
+            href_value = author_block.find('a', class_='mem_link')['href']
+            author_link = self.get_user_id(href_value)
+            message_text = self.get_message(author_block)
+            message_date = self.get_message_date(block)
+            attachment_links = self.get_attachment_links(block)
+            # print(f'Имя: {author_name}\nСсылка: {author_link}\nСообщение: {message_text}\n\Дата: {message_date}\n{attachment_links}\n')
+            return {
+                "author_name": author_name,
+                "author_link": author_link,
+                "message_text": message_text,
+                "message_date": message_date,
+                "attachment_links": attachment_links
+            }
+        except Exception as e:
+            print(f'Ошибка при обработке блока: {e}')
+
+class MessageFileProcessor(MessageProcessor):
+    def __init__(self):
+        super().__init__()
+        self.details = {}  
+        self.for_append = []
+        self.id_msg = 1
+
+    def process_rar_file(self, rar_file, output_dir):
+        try:
+            rf = rarfile.RarFile(rar_file)
+            rf.extractall(output_dir)
+        except Exception as e:
+            print(f'Ошибка при разархивации файла {rar_file}: {e}')
+
+    def process_all_html_files(self, rar_file, output_dir):
+        # Создаем временную папку для извлечения содержимого архива
+        temp_dir = tempfile.mkdtemp()
+
+        # Распаковываем архив
+        self.process_rar_file(rar_file, temp_dir)
+
+        # Получаем список всех файлов с расширением 'htm' во всех подкаталогах временной папки
+        files = glob.glob(os.path.join(temp_dir, '**', '*.htm*'), recursive=True)
+
+        util = MessageParser()
+        sorted_files = sorted(files, key=lambda x: (os.path.dirname(x), util.extract_number(x)))
+
+        for file_path in tqdm(sorted_files, desc="Processing files", unit="file"):
+            try:
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    content = file.read()
+
+                    details_chat = DetailsChat()
+                    self.details.update(details_chat.details(file_path))
+
+                    soup = BeautifulSoup(content, 'html.parser')
+                    im_in_blocks = self.get_im_in_blocks(soup)
+                    for block in im_in_blocks:
+                        self.id_msg = self.id_msg + 1
+                        details_copy = self.details.copy()
+                        details_copy.update({'id': self.id_msg})
+                        details_copy.update(self.process_block(block))
+                        self.for_append.append(details_copy)
+            except Exception as e:
+                print(f'Ошибка при обработке файла {file_path}: {e}')
+
+        # Удаляем временную папку после использования
+        shutil.rmtree(temp_dir)
+
+        return self.for_append
+class MessageDatabase:
+    def __init__(self, db_name, table_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.table_name = table_name
+        self.create_table()
+
+    def create_table(self):
+        self.cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {self.table_name} (
+                id INTEGER NOT NULL,  
+                chat_id TEXT,
+                file_chat TEXT,
+                author_name TEXT,
+                author_link TEXT,
+                message_text TEXT,
+                message_date TEXT,
+                attachment_links TEXT
+            )
+        ''')
+
+    def insert_data(self, json_data):
+        for item in json_data:
+            self.cursor.execute(f'''
+                INSERT INTO {self.table_name} (
+                    id, chat_id, file_chat, author_name, author_link, message_text, message_date, attachment_links
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (
+                item['id'],
+                item['chat_id'],
+                item['file_chat'],
+                item['author_name'],
+                item['author_link'],
+                item['message_text'],
+                item['message_date'],
+                json.dumps(item.get('attachment_links', []))
+            ))
+
+    def commit_and_close(self):
+        self.conn.commit()
+        self.conn.close()
+
+# Пример использования
+# rar_file_path = 'DUMP.rar'
+# output_dir = 'DUMP'
+# processor = MessageFileProcessor()
+# json_data = processor.process_all_html_files(rar_file_path, output_dir)
+# db = MessageDatabase("messages.db", "dump")
+# db.insert_data(json_data)
+# db.commit_and_close()
